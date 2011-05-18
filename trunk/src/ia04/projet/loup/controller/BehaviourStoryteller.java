41f949a68d22fb9cfea5a8b9b97a9bf1d0ad968e
aurelien.meunier.utc@gmail.com <aurelien.meunier.utc@gmail.com@16addc26-d16c-1d31-f19b-d9deca5dc3d6>
2011-05-18 02:08:15 +0000

    Debut de squelette pour AgtStoryteller et AgtPlayer (et leurs behaviours)

--- trunk/src/ia04/projet/loup/controller/BehaviourStoryteller.java
+++ trunk/src/ia04/projet/loup/controller/BehaviourStoryteller.java
@@ -1,18 +1,42 @@
 package ia04.projet.loup.controller;
 
 import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
 
 public class BehaviourStoryteller extends Behaviour {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1058995384097785392L;
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-
+		ACLMessage msg = myAgent.receive();
+		if(msg != null){
+			// Reception du message
+			String msgString = msg.getContent();
+			int msgPerformative = msg.getPerformative();
+			// TODO:
+			
+			// A - Le message peut venir d'un AgtPlayer:			
+			// 1. Enregistrement d'un joueur aupres de myAgent
+			// 2. Desinscription d'un joueur deja inscrit
+			// 3. Lancement d'une partie, participation ou non du joueur a la partie
+			// 4. Eventuelles reponses pendant l'attribution des roles (ex. voleur)
+			// 5. Apres indication des roles, joueur previent s'il est pret
+			
+			// B - Le message peut venir d'un AgtCommunication (Vote / Advice / Action)
+			// 1. Reponse a une action
+			// 2. Reponse a un vote 
+		}
 	}
 
 	@Override
+	/**
+	 * Our Storyteller agent will be running constantly. 
+	 * Its behaviour should also be running at all times.
+	 */
 	public boolean done() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
