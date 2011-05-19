f26006a5e455bf2447f87a2baa4ba51d4856a546
mathieu.viel@gmail.com <mathieu.viel@gmail.com@16addc26-d16c-1d31-f19b-d9deca5dc3d6>
2011-05-19 23:28:42 +0000

    Début de remplissage des classes AgtRole et BehaviourRole Ajout de la classe BehaviourVillager

--- trunk/src/ia04/projet/loup/roles/AgtRole.java
+++ trunk/src/ia04/projet/loup/roles/AgtRole.java
@@ -1,7 +1,52 @@
 package ia04.projet.loup.roles;
 
+import ia04.projet.loup.Global;
 import jade.core.Agent;
 
-public class AgtRole extends Agent {
+/**
+ * This agent is the core of a role. Each role agent has to be a subclass of AgtRole.
+ * It implements the main actions of a role: begining/end of a game, death of the player, etc...
+ * @author claquette
+ *
+ */
 
+public abstract class AgtRole extends Agent {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1226925844951644365L;
+	private Global.Strategies currentStrategy;
+	/**
+	 * The default constructor. Starts the agent and attach its behaviour.
+	 * Gets an acces to his player's GUI
+	 */
+	public AgtRole() {
+		super();
+		this.addBehaviour(new BehaviourRole());
+		initializeConfidenceLevel();
+		//TODO get the GUI
+	}
+	
+	/**
+	 * Initializes the different level of confidence at the beginning of a new game.
+	 * alpha - no confidence level
+	 * TODO beta2 - first implementation
+	 * TODO beta4 - initial state depending on previous games
+	 * 
+	 */
+	protected void initializeConfidenceLevel(){
+		
+	}
+	
+	/**
+	 * during the day every role has the same action to do :
+	 * vote to kill somebody in the village 
+	 */
+	protected void vote(){
+		switch (currentStrategy){
+		case RABBIT:
+			//TODO random vote
+		}
+	}
 }
