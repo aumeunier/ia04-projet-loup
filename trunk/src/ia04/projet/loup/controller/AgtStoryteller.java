ae34c679355c4fd183e2ec3d861aa7e4e06b409a
aurelien.meunier.utc@gmail.com <aurelien.meunier.utc@gmail.com@16addc26-d16c-1d31-f19b-d9deca5dc3d6>
2011-05-21 23:11:51 +0000

    Added skip / endOfGame / isGameOver

--- trunk/src/ia04/projet/loup/controller/AgtStoryteller.java
+++ trunk/src/ia04/projet/loup/controller/AgtStoryteller.java
@@ -33,8 +33,7 @@ public class AgtStoryteller extends Agent {
 		/** There aren't enough players to play this game right now. */
 		TOO_FEW_PLAYERS,
 		/** Some people won or everyone died. Either way, this game is over. */
-		GAME_OVER,
-		BOOM // TODO: remove
+		GAME_OVER
 	}
 	private static final long serialVersionUID = -1537520826022941930L;
 	/** The map of the players with their role. */
@@ -42,6 +41,7 @@ public class AgtStoryteller extends Agent {
 	private ArrayList<AID> lastVictimsRoles;
 	/** The clock that regulates the game phases and game speed. */
 	private PhaseClock phaseClock;
+	//TODO: lovers, charmed
 	
 	/**
 	 * The default constructor. Start an AgtStoryteller.
@@ -60,15 +60,72 @@ public class AgtStoryteller extends Agent {
 		this.phaseClock.startTimer();
 		this.playersMap = new HashMap<AID,Roles>();
 		this.lastVictimsRoles = new ArrayList<AID>();
+		
+		// TODO: test
+		int i = 0;
+		this.playersMap.put(new AID("test"+(i++)), Roles.WEREWOLF);
+		this.playersMap.put(new AID("test"+(i++)), Roles.WEREWOLF);
+		this.playersMap.put(new AID("test"+(i++)), Roles.WEREWOLF);
+		this.playersMap.put(new AID("test"+(i++)), Roles.VILLAGER);
+		this.playersMap.put(new AID("test"+(i++)), Roles.VILLAGER);
+		this.playersMap.put(new AID("test"+(i++)), Roles.VILLAGER);
+		this.playersMap.put(new AID("test"+(i++)), Roles.WITCH);
+		this.playersMap.put(new AID("test"+(i++)), Roles.CLAIRVOYANT);
 	}
 	
+	/**
+	 * Add a player to the party room. The players do not necessarily have to be "playing".
+	 * They can watch the game and participate in a game if a new game starts.
+	 * @param player The AID of the new player
+	 */
 	public void addPlayerToParty(AID player){
-		this.playersMap.put(player, null);
+		this.playersMap.put(player, Roles.DEAD);
 	}
+	/**
+	 * Give a role to a player
+	 * @param role The role to give to the player
+	 * @param player The player to which we want to give the role
+	 */
 	public void addRoleToPlayer(Roles role, AID player){
+		// TODO: Send message to the player
 		this.playersMap.put(player, role);
 	}
-	
+
+	/**
+	 * Game is over the there is no werewolf alive
+	 * or there are only werewolves alive 
+	 * or only the two lovers are alive
+	 * or all the players have been charmed
+	 */
+	public void checkGameIsOver(){
+		int nWolf = 0, nMax = 0, nCharmed = 0;		
+		for(Roles role: this.playersMap.values()){
+			switch(role){
+			case WEREWOLF: 
+				nWolf++;
+				nMax++;
+				break;
+			case WHITE_WOLF:
+				nWolf++;
+				nMax++;
+				break;
+			case DEAD:
+				break;
+			default:
+				nMax++;
+			}
+			// TODO: if charmed -> nCharmed++
+		}
+		
+		if((nWolf == nMax) || (nWolf == 0) || (nCharmed == nMax-1)){ // TODO: ||Êtwo lovers only
+			this.endGameWithState(GameExitErrorCodes.GAME_OVER);
+		}
+	}
+	/**
+	 * Check at least one player has the given role
+	 * @param role The role to search for
+	 * @return Whether one player at least has the given role
+	 */
 	public boolean roleStillInGame(Roles role){
 		return this.playersMap.containsValue(role);
 	}
@@ -80,62 +137,69 @@ public class AgtStoryteller extends Agent {
 	 * @return whether it should be skipped or not
 	 */
 	public boolean shouldSkipPhase(GamePhases phase){
-		// TODO: is the role still here ?
 		boolean shouldSkip = false;
 		boolean afterFirstRound = this.phaseClock.getNbOfTurns() > 1;
 		switch(phase){ // We only need to do phase x..
-		case CUPID: // during the first round and if someone has cupid role
+		case CUPID: // during the first round and if someone has the cupid role
 			shouldSkip = afterFirstRound || !roleStillInGame(Roles.CUPID);
 			break;
 		case LOVERS: // during the first round and if lovers were chosen
-			shouldSkip = afterFirstRound || !roleStillInGame(Roles.LOVERS);
+			// TODO: lovers and not cupid
+			shouldSkip = afterFirstRound || !roleStillInGame(Roles.CUPID);
 			break;
-		case THIEF: // during the first round and if someone has cupid role
+		case THIEF: // during the first round and if someone has the cupid role
 			shouldSkip = afterFirstRound || !roleStillInGame(Roles.THIEF);
 			break;
-		case GUARDIAN:
+		case GUARDIAN: // if someone has the guardian role
 			shouldSkip = !roleStillInGame(Roles.GUARDIAN);
 			break;
-		case CLAIRVOYANT:
+		case CLAIRVOYANT: // if someone has the clairvoyant role
 			shouldSkip = !roleStillInGame(Roles.CLAIRVOYANT);
 			break;
-		case WEREWOLVES:
-			shouldSkip = !roleStillInGame(Roles.WEREWOLF);
+		case WEREWOLVES: // if there is at least one were-wolf (otherwise isGameOver==true)
 			break;
-		case WITCH:
+		case WITCH: // if someone has the witch role
 			shouldSkip = !roleStillInGame(Roles.WITCH);
 			break;
-		case WHITE_WOLF:
+		case WHITE_WOLF: // if someone has the white-wolf role
 			shouldSkip = !roleStillInGame(Roles.WHITE_WOLF);
 			break;
-		case RAVEN:
+		case RAVEN: // if someone has the raven role
 			shouldSkip = !roleStillInGame(Roles.RAVEN);
 			break;
-		case FLUTE_PLAYER:
+		case FLUTE_PLAYER: // if someone has the flute player role
 			shouldSkip = !roleStillInGame(Roles.FLUTE_PLAYER);
 			break;
-		case CHARMED:
+		case CHARMED: // if someone has been charmed
+			// TODO: charmed
 			shouldSkip = !roleStillInGame(Roles.FLUTE_PLAYER);
 			break;
-		case VICTIMS_REVELATION:		
+		case VICTIMS_REVELATION: // if someone has been killed during the night
 			shouldSkip = this.lastVictimsRoles.isEmpty();
 			break;
-		case VICTIMS_EVENT:
-			// TODO: si la victime n'est pas amoureux chasseur ancien
-			shouldSkip = !roleStillInGame(Roles.LOVERS) &&
-						!roleStillInGame(Roles.HUNTER) &&
-						!roleStillInGame(Roles.VILLAGE_SAGE);	
+		case VICTIMS_EVENT: // if there is a lover, hunter or village sage night victim
+			shouldSkip = (this.lastVictimsRoles.contains(Roles.HUNTER)
+					//this.lastVictimsRoles.contains(Roles.LOVERS) //TODO:					
+					|| this.lastVictimsRoles.contains(Roles.VILLAGE_SAGE));
 			break;
-		case MAYOR_ELECTION:
+		case VICTIMS_RESOLUTION:
+			break;
+		case MAYOR_ELECTION: // if it is the first round
 			shouldSkip = afterFirstRound;
 			break;
-		case HANGED_VOTE:
+		case HUNG_VOTE:
 			break;
-		case HANGED_REVELATION:
+		case HUNG_REVELATION:
 			break;
-		case HANGED_EVENT:
+		case HUNG_EVENT: // if the hung one is a scapegoat, hunter, lover, idiot or sage
+			shouldSkip = (this.lastVictimsRoles.contains(Roles.SCAPEGOAT)
+					|| this.lastVictimsRoles.contains(Roles.HUNTER)
+					//|| this.lastVictimsRoles.contains(Roles.LOVERS) //TODO:
+					|| this.lastVictimsRoles.contains(Roles.VILLAGE_IDIOT)
+					|| this.lastVictimsRoles.contains(Roles.VILLAGE_SAGE));
 			break;
-		case HANGED_RESOLUTION:
+		case HUNG_RESOLUTION: // if someone was hung
+			shouldSkip = this.lastVictimsRoles.isEmpty();
 			break;
 		default:
 			break;
@@ -147,13 +211,13 @@ public class AgtStoryteller extends Agent {
 	 * @param phase The phase to begin.
 	 */
 	public void willStartPhase(GamePhases phase){
-		// TODO: lots of stuff
+		// TODO: lots of stuff to do
 		System.out.print("Storyteller says: ");
 		switch(phase){
 		case NONE:
 			break;
 		case NIGHT:
-			System.out.println("\nNEW TURN\nIt is now the night. The village goes to sleep.");
+			System.out.println("NEW TURN\n\nIt is now the night. The village goes to sleep.");
 			break;
 		case CUPID:
 			System.out.println("Cupid wakes up. He can choose two people who will deeply fall in love.");
@@ -198,20 +262,23 @@ public class AgtStoryteller extends Agent {
 		case VICTIMS_EVENT:
 			System.out.println("Before their last action the victims can try a desperate move.");
 			break;
+		case VICTIMS_RESOLUTION:
+			System.out.println("The victims die.");
+			break;
 		case MAYOR_ELECTION:
 			System.out.println("The mayor election can begin. The village needs someone to follow! " +
 					"Choose wisely because the mayor has power.");
 			break;
-		case HANGED_VOTE:
+		case HUNG_VOTE:
 			System.out.println("The hanged selection begins. Who will be hung on the place today ?");
 			break;
-		case HANGED_REVELATION:
+		case HUNG_REVELATION:
 			System.out.println("The hung role's revealed.");
 			break;
-		case HANGED_EVENT:
+		case HUNG_EVENT:
 			System.out.println("Before his hunging the hung can try a desperate move.");
 			break;
-		case HANGED_RESOLUTION:
+		case HUNG_RESOLUTION:
 			System.out.println("The hung is dead.");
 			break;
 		default:
@@ -219,12 +286,12 @@ public class AgtStoryteller extends Agent {
 		}		
 	}
 	/**
-	 * This method is called by the internal clock when the current phase should stop.
+	 * This method is called by the internal clock when the current phase stops.
 	 * The clock is actually stopped until the Storyteller does something.
 	 * @param phase The phase that should be ending.
 	 */
-	public void shouldEndPhase(GamePhases phase){
-		// TODO: lots of stuff
+	public void endOfPhase(GamePhases phase){
+		// TODO: lots of stuff to do
 		switch(phase){
 		case NONE:
 			break;
@@ -252,6 +319,7 @@ public class AgtStoryteller extends Agent {
 		case FLUTE_PLAYER:
 			break;
 		case CHARMED:
+			this.checkGameIsOver();
 			break;
 		case DAY:
 			break;
@@ -259,25 +327,25 @@ public class AgtStoryteller extends Agent {
 			break;
 		case VICTIMS_EVENT:
 			break;
+		case VICTIMS_RESOLUTION:
+			this.checkGameIsOver();
+			break;
 		case MAYOR_ELECTION:
 			break;
-		case HANGED_VOTE:
+		case HUNG_VOTE:
+			lastVictimsRoles.clear();
 			break;
-		case HANGED_REVELATION:
+		case HUNG_REVELATION:
 			break;
-		case HANGED_EVENT:
+		case HUNG_EVENT:
 			break;
-		case HANGED_RESOLUTION:
+		case HUNG_RESOLUTION:
+			this.checkGameIsOver();
 			break;
 		default:
 			break;
 		}	
-		if(this.phaseClock.getNbOfTurns() > 10){
-			this.endGameWithState(GameExitErrorCodes.BOOM);
-		}
-		else {
-			phaseClock.startNextPhase();
-		}
+		phaseClock.startNextPhase();
 	}
 	/**
 	 * Method called when the current game should stop. 
@@ -287,7 +355,7 @@ public class AgtStoryteller extends Agent {
 		this.phaseClock.stopTimer();
 		switch(errorCode){
 		case UNKNOWN_REASON:
-			System.out.println("I don't know why the game stopped. And I fuck you all!");
+			System.out.println("I don't know why the game stopped.");
 			break;
 		case BUG:
 			System.out.println("Nyeeeee... There is a problem here. Sorry for the inconvenience =(");
@@ -298,12 +366,19 @@ public class AgtStoryteller extends Agent {
 		case TOO_FEW_PLAYERS:
 			System.out.println("Hey guys! We need more people!");
 			break;
-		case GAME_OVER:
-			System.out.println("Game is over! Next game in 3..2..1.... ... .. .");
-			break;
-		case BOOM:
-			System.out.println("Bomb on the village. You die. And if you're not ok with that you die either way.");
-			break;
+		case GAME_OVER:{
+			//TODO: all charmed
+			//TODO: lovers
+			if(this.playersMap.containsValue(Roles.WEREWOLF)){
+				System.out.println("The werewolves win the game!");
+			}
+			else if(this.playersMap.containsValue(Roles.WHITE_WOLF)){
+				System.out.println("The white wolf wins the game!");
+			}
+			else {
+				System.out.println("The villagers win the game!");
+			}
+		}break;
 		default:
 			System.out.println("Laule. I don't even have an error code for that.");
 			break;
@@ -350,57 +425,4 @@ public class AgtStoryteller extends Agent {
 		Jmsg +=" }";
 		this.send(msg);
 	}
-	
-	
-	
-	/*
-	 * 
-		switch(phase){
-		case NONE:
-			break;
-		case NIGHT:
-			break;
-		case CUPID:
-			break;
-		case LOVERS:
-			break;
-		case THIEF:
-			break;
-		case GUARDIAN:
-			break;
-		case CLAIRVOYANT:
-			break;
-		case WEREWOLVES:
-			break;
-		case WITCH:
-			break;
-		case WHITE_WOLF:
-			break;
-		case RAVEN:
-			break;
-		case FLUTE_PLAYER:
-			break;
-		case CHARMED:
-			break;
-		case DAY:
-			break;
-		case VICTIMS_REVELATION:
-			break;
-		case VICTIMS_EVENT:
-			break;
-		case MAYOR_ELECTION:
-			break;
-		case HANGED_VOTE:
-			break;
-		case HANGED_REVELATION:
-			break;
-		case HANGED_EVENT:
-			break;
-		case HANGED_RESOLUTION:
-			break;
-		default:
-			break;
-		}	
-	 * 
-	 */
 }
