794fd0c67184e984f8127cc0602e65c5611b69d6
guillaume.swiatek@gmail.com <guillaume.swiatek@gmail.com@16addc26-d16c-1d31-f19b-d9deca5dc3d6>
2011-05-21 20:06:00 +0000

    Updated AgtPlayer

--- trunk/src/ia04/projet/loup/players/AgtPlayer.java
+++ trunk/src/ia04/projet/loup/players/AgtPlayer.java
@@ -1,17 +1,39 @@
 package ia04.projet.loup.players;
 
-import jade.gui.GuiAgent;
-import jade.gui.GuiEvent;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
 
 
-public class AgtPlayer extends GuiAgent {
+public class AgtPlayer extends Agent {
+	
 	private static final long serialVersionUID = -3215896432211766320L;
 
-	@Override
-	protected void onGuiEvent(GuiEvent arg0) {
-		// TODO:
+	public AgtPlayer(){
+		super();
+		this.addBehaviour(new BehaviourPlayer());
 	}
 	
+	public void Register(AID stryTeller){
+		//Send identification to storyTeller
+		String Jmsg ="{type : REGISTER}";
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		msg.setContent(Jmsg);
+		msg.addReceiver(stryTeller);
+		this.send(msg);
+	}
 	
+	public void JoinGame(ACLMessage msg, String value){
+		//Accept to join the beginning game
+		ACLMessage response = msg.createReply();
+		String Jmsg = "{type : REGISTER , value :" + value +"}";
+		response.setContent(Jmsg);
+		this.send(response);
+	}
 
+	public void RoleInstance(){
+		//RoleAgent instanciation
+		
+	}
+	
 }
