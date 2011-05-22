78179c42ba506700de6f251c9bcb365ba2894124
paul.cervera.y.alvarez <paul.cervera.y.alvarez@gmail.com>
2011-05-22 21:38:22 +0000

    Ajout des méthodes de serialization des mMessage Ajout de la DTO mVote

--- src/ia04/projet/loup/messages/mMessage.java
+++ src/ia04/projet/loup/messages/mMessage.java
@@ -1,15 +1,74 @@
 package ia04.projet.loup.messages;
 
-public class mMessage implements IMessage{
-	public static void parseJson(String jsonString){
+import java.io.StringWriter;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+/**
+ * This class provides the basic methods to serialize and deserialize an mMessage instance
+ * @author paul
+ */
+public abstract class mMessage implements IMessage{
+	
+	/**
+	 * Transform the Json String into a mMessage instance
+	 * @param jsonString
+	 * @param aClass specify a class which must extend mMessage
+	 * @return mMessage
+	 */
+	public static mMessage parseJson(String jsonString, Class<?> aClass){
 		
+		ObjectMapper m = new ObjectMapper();
+		mMessage r = null;
+	
+		try
+		{
+			r = (mMessage)m.readValue(jsonString, aClass);
+		}
+		catch(Exception e)
+		{
+			System.out.println("mMessage.parseJson - error : " + e);
+			return null;
+		}
+		
+		return r;
 	}
 
+	
+	/**
+	 * Transform the message to a message that can be sent
+	 * @return A JSON string representing the message content
+	 */
 	@Override
 	public String toJson() {
+		StringWriter sw = new StringWriter();
+		ObjectMapper m = new ObjectMapper();
+		
+		try
+		{
+			m.writeValue(sw, this);
+			return sw.toString();
+		}
+		catch(Exception e)
+		{
+			System.out.println("mMessage.toJson() - error : " + e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Transform the message to a human-understandable form
+	 * @return A string human-readable representation of the message
+	 */
+	public String toString(){
 		return null;
 	}
 
+	/**
+	 * Transform the JSON string given to a human-understandable string.
+	 * @param jsonString The JSON string to transform
+	 * @return A string that you and me can read easily
+	 */
 	@Override
 	public String jsonToString(String jsonString) {
 		return null;
---
+++ src/ia04/projet/loup/messages/mVote.java
@@ -0,0 +1,98 @@
+package ia04.projet.loup.messages;
+
+import jade.core.AID;
+
+/**
+ * This class is used to create a vote message between the AgtVote and the AgtRoles
+ * @author paul
+ */
+public class mVote extends mMessage{
+	
+	public static enum mType {
+		KILL_PAYSAN, KILL_WW, ELECT_MAYOR, SUCCESSOR 
+	}
+	
+	/**
+	 * Type of the election
+	 */
+	private mType type;
+	
+	/**
+	 * Number of voices
+	 * Useful when the mayor votes
+	 */
+	private int numbreOfVoices;
+	
+	/**
+	 * LocalName of the chosen AgtRole
+	 */
+	private String choice;
+	
+	/**
+	 * Constructor
+	 */
+	public mVote(){
+		this.numbreOfVoices = 1;
+	}
+	
+	/**
+	 * Constructor
+	 * @param aType
+	 */
+	public mVote(mType aType){
+		this.type = aType;
+		this.numbreOfVoices = 1;
+	}
+	
+	/**
+	 * Return an instance of mVote build on a JSON string
+	 * @param jsonString
+	 * @return mVote
+	 */
+	public static mVote parseJson(String jsonString){
+		return (mVote)mMessage.parseJson(jsonString, mVote.class);
+	}
+	
+	/**
+	 * @param numbreOfVoices the numbreOfVoices to set
+	 */
+	public void setNumbreOfVoices(int numbreOfVoices) {
+		this.numbreOfVoices = numbreOfVoices;
+	}
+
+	/**
+	 * @return the numbreOfVoices
+	 */
+	public int getNumbreOfVoices() {
+		return numbreOfVoices;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(mType type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public mType getType() {
+		return type;
+	}
+
+	/**
+	 * @param choice the choice to set
+	 */
+	public void setChoice(String choice) {
+		this.choice = choice;
+	}
+
+	/**
+	 * @return the choice
+	 */
+	public String getChoice() {
+		return choice;
+	}
+	
+}
