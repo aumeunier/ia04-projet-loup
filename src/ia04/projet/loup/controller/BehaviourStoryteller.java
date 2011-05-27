969245a991f44708dee3211d73f33172087a5fe3
aurelien.meunier.utc <aurelien.meunier.utc@gmail.com>
2011-05-27 18:33:10 +0000

    Communication between Storyteller and Player: - Storyteller can now act accordingly when a Player ask for Registration - Storyteller can now ask Players whether they want to start the game - Storyteller can now get the results of the previous question and start the game if possible Global: added role "Unassigned" to be used by the storyteller when he remembers the player who want to start the game

--- src/ia04/projet/loup/controller/BehaviourStoryteller.java
+++ src/ia04/projet/loup/controller/BehaviourStoryteller.java
@@ -1,13 +1,15 @@
 package ia04.projet.loup.controller;
 
+import ia04.projet.loup.Global.Roles;
 import ia04.projet.loup.messages.mMessage;
+import ia04.projet.loup.messages.mStorytellerCommunication;
 import ia04.projet.loup.messages.mStorytellerPlayer;
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
 
 public class BehaviourStoryteller extends Behaviour {
 	private static final long serialVersionUID = 1058995384097785392L;
-	
+
 	public BehaviourStoryteller(AgtStoryteller a){
 		super(a);
 	}
@@ -19,19 +21,47 @@ public class BehaviourStoryteller extends Behaviour {
 			// Message reception
 			String msgString = msg.getContent();
 			int msgPerformative = msg.getPerformative();
-			// TODO:
-			//System.out.println(msgString);
-			
-			// A - Message can come from an AgtPlayer:			
-			// 1. Player's registration
-			// 2. Player's unregistration
-			// 3. Participation in the new game
-			// 4. Eventual answers to role attribution
-			// 5. Is the player ready to play (has finished role initialization)
+
+			// A - Message can come from an AgtPlayer:	
+			mMessage generalMessage = mMessage.parseJson(msgString, mStorytellerPlayer.class);
+			if(generalMessage != null){
+				mStorytellerPlayer message = (mStorytellerPlayer)generalMessage;
+				// 1. Player's registration
+				if(message.type.equals(mStorytellerPlayer.mType.REGISTER)) {
+					System.out.println(msg.getSender().getLocalName()+" registered.");
+					((AgtStoryteller)(this.myAgent)).addPlayerToParty(msg.getSender());
+				}
+				
+				// 2. Player's unregistration
+				else if(message.type.equals(mStorytellerPlayer.mType.LEAVE_GAME)) {
+					//TODO:
+				}
+				
+				// 3. Participation in the new game
+				else if(message.type.equals(mStorytellerPlayer.mType.START_GAME)) {
+					System.out.println(msg.getSender().getLocalName()+" wants to participate in the game.");
+					((AgtStoryteller)(this.myAgent)).addRoleToPlayer(Roles.UNASSIGNED, msg.getSender());
+				}
+				
+				// 4. Eventual answers to role attribution
+				else if(message.type.equals(mStorytellerPlayer.mType.ATTRIBUTE_ROLE)) {		
+					//TODO:			
+				}
+				
+				// 5. Is the player ready to play (has finished role initialization)
+				else if(message.type.equals(mStorytellerPlayer.mType.LEAVE_GAME)) {
+					//TODO:
+				}
+
+			}
 			
 			// B - Message can come from an AgtCommunication (Vote / Advice / Action)
-			// 1. Answer to an action
-			// 2. Answer to a vote 
+			else {
+				generalMessage = mMessage.parseJson(msgString, mStorytellerCommunication.class);
+				// 1. Answer to an action
+				// 2. Answer to a vote 
+			}
+
 		}
 	}
 
