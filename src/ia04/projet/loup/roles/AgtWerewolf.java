f3684a862a11646492d37c487b0d1d50ca3a8afe
mathieu.viel <mathieu.viel@gmail.com>
2011-06-02 07:32:46 +0000

    ajout du performative inform sur les messages de vote

--- src/ia04/projet/loup/roles/AgtWerewolf.java
+++ src/ia04/projet/loup/roles/AgtWerewolf.java
@@ -1,5 +1,6 @@
 package ia04.projet.loup.roles;
 
+import ia04.projet.loup.Debugger;
 import ia04.projet.loup.Global.Roles;
 import ia04.projet.loup.messages.mVote;
 
@@ -15,6 +16,7 @@ public class AgtWerewolf extends AgtRole {
 	
 	public AgtWerewolf () {
 		super();
+		Debugger.println("AgtWerewofl: constructor");
 		addAndSaveBehaviour(new BehaviourWerewolf());
 	}
 	/** initialize the role of the agent */
@@ -25,6 +27,7 @@ public class AgtWerewolf extends AgtRole {
 	public String eatSomebody(ArrayList<String> candidates){
 		switch (currentStrategy){
 		case RABBIT:
+			Debugger.println("AgtWerewofl: eatSomedy-RABBIT");
 			Random random = new Random();
 			return candidates.get(random.nextInt(candidates.size()));
 		default: return null;
