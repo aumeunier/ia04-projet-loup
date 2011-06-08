a97a1babffe6b851425e2cbf345c492b522b48a0
mathieu.viel@gmail.com <mathieu.viel@gmail.com@16addc26-d16c-1d31-f19b-d9deca5dc3d6>
2011-06-08 13:55:21 +0000

    implementation de mPlayerRole

--- trunk/src/ia04/projet/loup/messages/mPlayerRole.java
+++ trunk/src/ia04/projet/loup/messages/mPlayerRole.java
@@ -1,6 +1,38 @@
 package ia04.projet.loup.messages;
 
+import ia04.projet.loup.roles.ConfidenceLevel;
+import jade.util.leap.HashMap;
+
+import java.util.ArrayList;
+
 public class mPlayerRole extends mMessage {
-	public String type = "DIE";
+	/** Message between player and his role containing the confidence levels */
+	public static enum mType {
+		/** This message is used at the begining or the end of the game */
+		START,END
+		}
+	private java.util.HashMap<String, ConfidenceLevel> confidenceLevel;
+	private mType type;
+	
+	public mType getType() {
+		return type;
+	}
+
+	public void setType(mType type) {
+		this.type = type;
+	}
+
+	public mPlayerRole (){
+		super();
+	}
+
+	public java.util.HashMap<String, ConfidenceLevel> getConfidenceLevel() {
+		return confidenceLevel;
+	}
+
+	public void setConfidenceLevel(
+			java.util.HashMap<String, ConfidenceLevel> confidenceLevel) {
+		this.confidenceLevel = confidenceLevel;
+	}
 	
 }
