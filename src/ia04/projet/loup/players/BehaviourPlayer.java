10fea1072bfc81eaec1c42c931311afad71267a2
paul.cervera.y.alvarez <paul.cervera.y.alvarez@gmail.com>
2011-06-08 22:57:07 +0000

    Merge AgtPlayer et AgtPlayerGui

--- src/ia04/projet/loup/players/BehaviourPlayer.java
+++ src/ia04/projet/loup/players/BehaviourPlayer.java
@@ -1,105 +1,119 @@
 package ia04.projet.loup.players;
 
-import ia04.projet.loup.Debugger;
 import ia04.projet.loup.Global;
 import ia04.projet.loup.messages.mMessage;
 import ia04.projet.loup.messages.mPlayerDied;
 import ia04.projet.loup.messages.mPlayerRole;
 import ia04.projet.loup.messages.mStorytellerPlayer;
-import ia04.projet.loup.messages.mToGui;
 import jade.core.AID;
-import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.wrapper.StaleProxyException;
 
-public class BehaviourPlayer extends Behaviour {
+public class BehaviourPlayer extends CyclicBehaviour {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	/** Wait messages then acts in consequence
+	/**
+	 * Wait messages then acts in consequences
+	 * 
 	 * @author Guillaume
 	 * */
 	@Override
 	public void action() {
+		
+		mPlayerRole msgRole = null;
+		mPlayerDied msgDied = null;
+		mStorytellerPlayer msgStoryTeller = null;
+		
 		ACLMessage msg = myAgent.receive();
-		if(msg != null){
-			// Message reception
-			String msgString = msg.getContent();
+		if (msg != null) {
+			String msgContent = msg.getContent();
 			// B - Message can come from an AgtRole
-			// 1. Reception of information concerning the db (stats, can be reused in future games)
+			// 1. Reception of information concerning the db (stats, can be
+			// reused in future games)
 
-			mPlayerRole msgRole = (mPlayerRole) mMessage.parseJson(msgString, mPlayerRole.class);
-			if(msgRole!=null){
-
-			}
-			else {
-				mPlayerDied msgDied = (mPlayerDied) mMessage.parseJson(msgString, mPlayerDied.class);
-				if(msgDied!=null){
-					boolean isDone = msgDied.getIsOver();
-					if(isDone){
-						// Role finished the dying stuff, notify AgtStoryteller
-						ACLMessage msgForRole = new ACLMessage(ACLMessage.INFORM);
-						msgForRole.setContent(msgDied.toJson());
-						AID aidStoryteller = new AID(Global.LOCALNAME_STORYTELLER,AID.ISLOCALNAME);
-						msgForRole.addReceiver(aidStoryteller);
-						myAgent.send(msgForRole);
-					}
-					else {
-						// Start doing the cleaning do to death, notify AgtRole
-						ACLMessage msgForRole = new ACLMessage(ACLMessage.INFORM);
-						msgForRole.setContent(msgDied.toJson());
-						msgForRole.addReceiver(((AgtPlayer) myAgent).getRoleID());
-						myAgent.send(msgForRole);
-					}
-				} 
-				else {
-					// C - Message can come from an AgtStoryteller:			
-					// 1. Inscription validation
-					// 2. Beginning of a game, participation request
-					// 3. Role attribution
-					// 4. Eventual additional messages concerning the role attribution
-					// 5. End of the game
-
-					mStorytellerPlayer msgObj = (mStorytellerPlayer) mMessage.parseJson(msgString, mStorytellerPlayer.class);
-					if(msgObj!=null){
-						switch(msgObj.getType()){
-						case START_GAME :
-							((AgtPlayer)myAgent).JoinGame(msg, msgObj);
-							break;
-						case ATTRIBUTE_ROLE :
-							try {
-								((AgtPlayer) myAgent).RoleInstance(msgObj.getRole());
-								ACLMessage reply = msg.createReply();
-								reply.setContent(msgObj.toJson());
-								((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.ROLE,msgObj.getRole());
-								((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STATUS,"ALIVE");
-								myAgent.send(reply);
-							} catch (StaleProxyException e) {
-								e.printStackTrace();
-							}
-							break;
-						case STORYTELLING :
-							((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STORYTELLING,msgObj.getStoryTelling());
-							break;
-						case END_GAME:
-							((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STORYTELLING,msgObj.getStoryTelling());
-							break;
-						default : break;
-						}			
-					}
-				}
+			if ((msgRole = (mPlayerRole) mMessage.parseJson(msgContent,mPlayerRole.class)) != null) {
+				handleMsgRole(msgRole);
+			}else if(null != (msgDied = (mPlayerDied) mMessage.parseJson(msgContent, mPlayerDied.class))) {
+				handleMsgDied(msgDied);
+			} else if((msgStoryTeller = (mStorytellerPlayer) mMessage.parseJson(msgContent, mStorytellerPlayer.class))!= null) {
+				handleMsgStoryTeller(msgStoryTeller, msg);
 			}
 		}
 	}
 
-	/** This is a cyclic behavior
-	 * */
-	@Override
-	public boolean done() {
-		return false;
+	/**
+	 * @todo TO COMPLETE
+	 * @param msgRole
+	 */
+	private void handleMsgRole(mPlayerRole msgRole){
+		
+	}
+	
+	
+	/**
+	 * @param msgStoryTeller
+	 * @param msg
+	 */
+	private void handleMsgStoryTeller(mStorytellerPlayer msgStoryTeller, ACLMessage msg) {
+		// C - Message can come from an AgtStoryteller:
+		// 1. Inscription validation
+		// 2. Beginning of a game, participation request
+		// 3. Role attribution
+		// 4. Eventual additional messages concerning the role
+		// attribution
+		// 5. End of the game
+
+		switch (msgStoryTeller.getType()) {
+			case START_GAME:
+				((AgtPlayer) myAgent).JoinGame(msg, msgStoryTeller);
+				break;
+			case ATTRIBUTE_ROLE:
+				try {
+					((AgtPlayer) myAgent).RoleInstance(msgStoryTeller.getRole());
+					ACLMessage reply = msg.createReply();
+					reply.setContent(msgStoryTeller.toJson());
+					//((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.ROLE, msgStoryTeller.getRole());
+					((AgtPlayer) myAgent).setRole(msgStoryTeller.getRole());
+					//((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STATUS, "ALIVE");
+					((AgtPlayer) myAgent).setStat("ALIVE");
+					myAgent.send(reply);
+				} catch (StaleProxyException e) {
+					e.printStackTrace();
+				}
+				break;
+			case STORYTELLING:
+				//((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STORYTELLING,msgStoryTeller.getStoryTelling());
+				((AgtPlayer) myAgent).setStoryView(msgStoryTeller.getStoryTelling());
+				break;
+			case END_GAME:
+//				((AgtPlayer) myAgent).TransfertToGui(mToGui.mType.STORYTELLING, msgStoryTeller.getStoryTelling());
+				((AgtPlayer) myAgent).setStoryView(msgStoryTeller.getStoryTelling());
+				break;
+			default:
+				break;
+		}
 	}
 
+	/**
+	 * @param msgDied
+	 */
+	private void handleMsgDied(mPlayerDied msgDied) {
+		boolean isDone = msgDied.getIsOver();
+		if (isDone) {
+			// Role finished the dying stuff, notify AgtStoryteller
+			ACLMessage msgForRole = new ACLMessage(ACLMessage.INFORM);
+			msgForRole.setContent(msgDied.toJson());
+			AID aidStoryteller = new AID(Global.LOCALNAME_STORYTELLER, AID.ISLOCALNAME);
+			msgForRole.addReceiver(aidStoryteller);
+			myAgent.send(msgForRole);
+		} else {
+			// Start doing the cleaning do to death, notify AgtRole
+			ACLMessage msgForRole = new ACLMessage(ACLMessage.INFORM);
+			msgForRole.setContent(msgDied.toJson());
+			msgForRole.addReceiver(((AgtPlayer) myAgent).getRoleID());
+			myAgent.send(msgForRole);
+		}
+	}
 }
